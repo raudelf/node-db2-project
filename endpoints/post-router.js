47dc8d2dd748d02136e4a734a1e20a73f9a17f6f
raudelf <rawdell@outlook.com>
2020-04-14 21:57:50 -0500

    Fixed my issue where data would still post even though Mileage had an empty string. I moved my if statement OUT of the db call so that it wouldn't run at all.

--- endpoints/post-router.js
+++ endpoints/post-router.js
@@ -6,18 +6,18 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const carData = req.body;
-
-    db('cars-migration').insert(carData)
+    if (carData.vin && carData.make && carData.model && carData.mileage)
+        {
+        db('cars-migration').insert(carData)
         .then(post => {
-            if (carData.vin && carData.make && carData.model && carData.mileage) {
                 res.status(201).json(post);
-            } else {
-                res.status(400).json({ message: 'You must include your car\'s Vin, Make, Model, and Mileage.'})
-            }
         })
         .catch(err => {
             res.status(500).json({ message: 'Error adding new car info', err});
         })
+    } else {
+            res.status(400).json({ message: 'You must include your car\'s Vin, Make, Model, and Mileage.'})
+        }
 });
 
 module.exports = router;
